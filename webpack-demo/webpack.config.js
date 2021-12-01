d09dffb2689bbadc16b18c42b09c8c72000c79da
周立学 <815066719@qq.com>
2021-12-01 10:09:10 +0800

    feat: webpack-entry\output\plugins

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bound.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  mode: 'none',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'app.html',
+      inject: 'body',
+    }),
+  ],
+};
