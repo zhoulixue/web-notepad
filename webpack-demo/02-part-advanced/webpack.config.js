d19cd46ce94a20630f32fca49840c40d7a689961
周立学 <815066719@qq.com>
2021-12-08 09:27:11 +0800

    feat: webpack-devServer
    1. compress
    2. port
    3. proxy
    4. https/http2
    5. historyApiFallback
    6. host

--- webpack-demo/02-part-advanced/webpack.config.js
+++ webpack-demo/02-part-advanced/webpack.config.js
@@ -11,6 +11,24 @@ module.exports = {
   // devtool: 'cheap-source-map', // 生成没有列信息的map文件
   devtool: 'cheap-module-source-map', // 开发环境下推荐使用
   output: {
+    publicPath: '/'
+  },
+  devServer: {
+    static: '/',
+    compress: true, // Content-Encoding: gzip;
+    port: 3000,
+    headers: {
+      'X-access-token': '123',
+    },
+    https: true,
+    http2: true,
+    proxy: {
+      '/api': 'http://localhost:9000',
+    },
+    historyApiFallback: true, // 跳转任意路由
+    host: '0.0.0.0',
+  },
+  output: {
     clean: true,
   },
   plugins: [
