a6d1bde5901513ced1c05bc16bdfe82192bdc39c
周立学 <815066719@qq.com>
2021-12-14 10:18:37 +0800

    feat: webpack -tree-shaking/sideEffects

---
+++ webpack-demo/02-part-advanced/main2.js
@@ -0,0 +1,2 @@
+import { add } from './math'
+console.log(add(5, 6))
--- webpack-demo/02-part-advanced/webpack.config.js
+++ webpack-demo/02-part-advanced/webpack.config.js
@@ -1,32 +1,33 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: {
-      import: ['./src/app.js'],
-      dependOn: 'lodash',
-      filename: 'channel1/[name].js'
-    },
-    main2: {
-      import: ['./src/app2.js'],
-      dependOn: 'lodash',
-      filename: 'channel2/[name].js'
-    },
-    lodash: {
-      import: 'lodash',
-      filename: 'common/[name].js'
-    }
-  },
+  mode: 'production',
+  entry: './main2.js',
+  // entry: {
+  //   main: {
+  //     import: ['./src/app.js'],
+  //     dependOn: 'lodash',
+  //     filename: 'channel1/[name].js'
+  //   },
+  //   main2: {
+  //     import: ['./src/app2.js'],
+  //     dependOn: 'lodash',
+  //     filename: 'channel2/[name].js'
+  //   },
+  //   lodash: {
+  //     import: 'lodash',
+  //     filename: 'common/[name].js'
+  //   }
+  // },
   // devtool: 'eval', // 默认值
   // devtool: 'source-map',
   // devtool: 'hidden-source-map', // 会生成map但是不会关联
   // devtool: 'inline-source-map', // 不会单独打包map文件
   // devtool: 'eval-source-map',
   // devtool: 'cheap-source-map', // 生成没有列信息的map文件
-  devtool: 'cheap-module-source-map', // 开发环境下推荐使用
+  // devtool: 'cheap-module-source-map', // 开发环境下推荐使用
   output: {
     publicPath: '/'
   },
@@ -54,22 +55,22 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'main1',
-      inject: 'body',
-      chunks: ['main', 'lodash'],
-      filename: 'channel1/index.html',
-      publicPath: 'http://www.a.com'
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'main2',
-      inject: 'body',
-      chunks: ['main2', 'lodash'],
-      filename: 'channel2/index.html',
-      publicPath: 'http://www.b.com'
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: './index.html',
+    //   title: 'main1',
+    //   inject: 'body',
+    //   chunks: ['main', 'lodash'],
+    //   filename: 'channel1/index.html',
+    //   publicPath: 'http://www.a.com'
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './index.html',
+    //   title: 'main2',
+    //   inject: 'body',
+    //   chunks: ['main2', 'lodash'],
+    //   filename: 'channel2/index.html',
+    //   publicPath: 'http://www.b.com'
+    // }),
     // new BundleAnalyzerPlugin(),
   ],
   module: {
@@ -102,4 +103,7 @@ module.exports = {
       'jQuery',
     ],
   },
+  // optimization: {
+  //   usedExports: true
+  // }
 };
