496cb9f95cbb7524f621ce45e22316de681c9c77
周立学 <815066719@qq.com>
2021-12-28 09:25:00 +0800

    feat: [nodejs] buffer child-process

--- nodejs-demo/01-part-basic/08-child-process.js
+++ nodejs-demo/01-part-basic/08-child-process.js
@@ -1,5 +1,5 @@
 const { spawn } = require('child_process');
-const ls = spawn('ls', ['-lh', 'usr']);
+const ls = spawn('ls', ['-lh', '/usr']);
 
 ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
--- nodejs-demo/old/fs-test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const readStream = fs.createReadStream('./data/output.txt');
-const writeStream = fs.createWriteStream('./data/output2.txt');
-
-readStream.pipe(writeStream);
